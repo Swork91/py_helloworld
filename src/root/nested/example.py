5b54bfb351050e951dd2d12809fa22ad6dea4c78
Swork91 <chicagomike999@gmail.com>
2018-04-30 14:08:01 -0400

    python garbage
    messing around with python in eclipse. Mostly eclipse, but eh

---
+++ src/root/nested/example.py
@@ -0,0 +1,45 @@
+'''
+Created on Apr 30, 2018
+
+@author: Sdxwo_000
+'''
+#I am a comment. For some reason my interpreter thinks that grey on white is a
+#good idea. I guess I know what their Eclipse theme is.
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+print('hello, world!')
+
+''' Nevermind I went through some themes.
+I'm darkest dark now. I guess thats the most popular. 
+And this multiline comment is also dumb. Why is it
+super visible like a string, but single line is 
+nearly impossible to read?
+'''
+
+""" single multiline """
+
+''' using single quotes! '''
+
+
