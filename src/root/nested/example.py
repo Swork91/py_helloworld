6554b041d0f1835f18a47732c4e7a8aa71134a1f
Swork91 <chicagomike999@gmail.com>
2018-04-30 15:22:31 -0400

    methods and such

--- src/root/nested/example.py
+++ src/root/nested/example.py
@@ -5,32 +5,22 @@ Created on Apr 30, 2018
 '''
 #I am a comment. For some reason my interpreter thinks that grey on white is a
 #good idea. I guess I know what their Eclipse theme is.
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
-print('hello, world!')
+print('is items 5????')
 
+items = 3
+GPA = 2.9
+dogs = "cute"
+
+if items == 5 & items > 20:
+    print (items)
+    print ("yes")
+else:
+    print ("ACTUALLY IT ")
+    print (items)
+
+print ("I think dogs are: " + dogs)
+
+    
 ''' Nevermind I went through some themes.
 I'm darkest dark now. I guess thats the most popular. 
 And this multiline comment is also dumb. Why is it
@@ -41,5 +31,23 @@ nearly impossible to read?
 """ single multiline """
 
 ''' using single quotes! '''
+vacation = False
+weekday = True
 
 
+
+def pork(v, w):
+    if (v == False and w == True):
+        return False
+    else:
+        return True
+    
+print(pork(vacation, weekday))
+
+def dork (v,w):
+    if not v and w:
+        return False
+    else:
+        return True
+    
+print(dork(vacation, weekday))
